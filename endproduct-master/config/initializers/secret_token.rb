9ac9ee7e0f996a513e74bff6002182adb288709e
Harry Convey <harry.convey@gmail.com>
2013-05-25 22:23:23 +0100

    here we go

---
+++ endproduct-master/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ConveySation::Application.config.secret_token = '9e5ad457cab60aac68aac9fa876a7095bd10ad254d3a7f9f74572743d3d4109648c7f4873a14e01c642a0a89c42fb9d68454c0e3729ca23a28106e91df980866'
